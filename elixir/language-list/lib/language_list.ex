2a0b55e8bedda20ae0264179cb8ffd2c3327783a
EmmaRamirez <ramirez.emma.g@gmail.com>
2022-06-04 13:24:43 -0600

    feat(elixir): add language list exercise

---
+++ elixir/language-list/lib/language_list.ex
@@ -0,0 +1,26 @@
+defmodule LanguageList do
+  def new() do
+    []
+  end
+
+  def add(list, language) do
+    [language | list]
+  end
+
+  def remove(list) do
+    [_head | tail] = list
+    tail
+  end
+
+  def first(list) do
+    List.first(list)
+  end
+
+  def count(list) do
+    length list
+  end
+
+  def functional_list?(list) do
+    "Elixir" in list
+  end
+end
